d12fd5bc1690cccc5dc5f1f43d909c526b0e3ed6
dmilagros <dmilagrosvelasquez@gmail.com>
2021-08-16 03:07:06 -0500

    action buttons

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,32 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useDispatch } from 'react-redux';
+import { loginAction, logoutAction } from '../redux/actions/login';
+import { addCounter, removeCounter } from '../redux/actions/ui';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const dispatch = useDispatch();
+
+  const handleLogin = () => {
+    dispatch(loginAction)
+  }
+  const handleLogout = () => {
+    dispatch(logoutAction)
+  }
+  const handleAddCounter = () => {
+    dispatch(addCounter)
+  }
+  const handleRemoveCounter = () => {
+    dispatch(removeCounter)
+  }
+
   return (
     <div className={styles.container}>
-      Welcome!
+      <button onClick={handleAddCounter}>+1</button>
+      <button onClick={handleRemoveCounter}>-1</button>
+      <button onClick={handleLogin}>LOGIN</button>
+      <button onClick={handleLogout}>LOGOUT</button>
     </div>
   )
 }
